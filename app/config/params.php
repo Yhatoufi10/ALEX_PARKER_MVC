e173dc54a25bd2232eebc4c2493e91078b88bc47
youssef hatoufi <hatoufiyoussef@gmail.com>
2025-11-06 17:31:52 +0100

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// PARAMS
+
+// Parametres de connexion a la DB
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'alex_parker_octobre_2021');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
